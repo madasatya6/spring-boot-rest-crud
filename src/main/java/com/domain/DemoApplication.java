ccc2856464a06acc940dda4dba8541f14d8dd8c8
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2024-11-20 05:27:19 +0700

    up

--- src/main/java/com/domain/DemoApplication.java
+++ src/main/java/com/domain/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// belajar di crud rest api = https://www.youtube.com/watch?v=Z0JfmObjKRw
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
